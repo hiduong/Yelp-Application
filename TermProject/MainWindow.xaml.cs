ae759e442d79bd312547d7250cbd30e258a5af5d
Hien Duong <hienduong931@gmail.com>
2021-04-01 14:14:10 -0700

    Done with g and h

--- TermProject/MainWindow.xaml.cs
+++ TermProject/MainWindow.xaml.cs
@@ -27,6 +27,11 @@ namespace TermProject
             public string name { get; set; }
             public string state { get; set; }
             public string city { get; set; }
+            public string address { get; set; }
+            public string zipcode { get; set; }
+            public string rating { get; set; }
+            public string tipcount { get; set; }
+            public string checkincount { get; set; }
         }
 
         public MainWindow()
@@ -76,26 +81,56 @@ namespace TermProject
             DataGridTextColumn col1 = new DataGridTextColumn();
             col1.Header = "BusinessName";
             col1.Binding = new Binding("name");
-            col1.Width = 255;
+            col1.Width = 201;
             ResultsGrid.Columns.Add(col1);
 
             DataGridTextColumn col2 = new DataGridTextColumn();
-            col2.Header = "City";
-            col2.Binding = new Binding("city");
-            col2.Width = 150;
+            col2.Header = "Address";
+            col2.Binding = new Binding("address");
+            col2.Width = 200;
             ResultsGrid.Columns.Add(col2);
 
             DataGridTextColumn col3 = new DataGridTextColumn();
-            col3.Header = "State";
-            col3.Binding = new Binding("state");
-            col3.Width = 80;
+            col3.Header = "City";
+            col3.Binding = new Binding("city");
+            col3.Width = 135;
             ResultsGrid.Columns.Add(col3);
 
             DataGridTextColumn col4 = new DataGridTextColumn();
-            col4.Header = "";
-            col4.Binding = new Binding("bid");
-            col4.Width = 200;
+            col4.Header = "State";
+            col4.Binding = new Binding("state");
+            col4.Width = 50;
             ResultsGrid.Columns.Add(col4);
+
+            DataGridTextColumn col5 = new DataGridTextColumn();
+            col5.Header = "Zipcode";
+            col5.Binding = new Binding("zipcode");
+            col5.Width = 60;
+            ResultsGrid.Columns.Add(col5);
+
+            DataGridTextColumn col6 = new DataGridTextColumn();
+            col6.Header = "Stars";
+            col6.Binding = new Binding("rating");
+            col6.Width = 40;
+            ResultsGrid.Columns.Add(col6);
+
+            DataGridTextColumn col7 = new DataGridTextColumn();
+            col7.Header = "# of Tips";
+            col7.Binding = new Binding("tipcount");
+            col7.Width = 60;
+            ResultsGrid.Columns.Add(col7);
+
+            DataGridTextColumn col8 = new DataGridTextColumn();
+            col8.Header = "Total\nCheckins";
+            col8.Binding = new Binding("checkincount");
+            col8.Width = 60;
+            ResultsGrid.Columns.Add(col8);
+
+            DataGridTextColumn col9 = new DataGridTextColumn();
+            col9.Header = "";
+            col9.Binding = new Binding("bid");
+            col9.Width = 200;
+            ResultsGrid.Columns.Add(col9);
         }
 
         private void AddState()
@@ -177,9 +212,12 @@ namespace TermProject
 
         private void AddButton_Click(object sender, RoutedEventArgs e)
         {
-            if (AddedListBox.Items.Contains(CategoryListBox.SelectedItem.ToString()) == false)
+            if (CategoryListBox.SelectedIndex > -1)
             {
-                AddedListBox.Items.Add(CategoryListBox.SelectedItem.ToString());
+                if (AddedListBox.Items.Contains(CategoryListBox.SelectedItem.ToString()) == false)
+                {
+                    AddedListBox.Items.Add(CategoryListBox.SelectedItem.ToString());
+                }
             }
         }
 
@@ -196,7 +234,7 @@ namespace TermProject
 
         private void AddGridRow(NpgsqlDataReader R)
         {
-            ResultsGrid.Items.Add(new Business() { name = R.GetString(0), city = R.GetString(1), state = R.GetString(2), bid = R.GetString(3) });
+            ResultsGrid.Items.Add(new Business() { name = R.GetString(0), address = R.GetString(1), city = R.GetString(2), state = R.GetString(3), zipcode = R.GetString(4), rating = R.GetDouble(5).ToString(), tipcount = R.GetInt16(6).ToString(), checkincount = R.GetInt16(7).ToString(), bid = R.GetString(8) });
         }
 
         private void SearchButton_Click(object sender, RoutedEventArgs e)
@@ -210,16 +248,37 @@ namespace TermProject
                     string temp = "";
                     foreach(string s in AddedListBox.Items)
                     {
-                        temp = temp + "categoryname='" + s + "'" + " AND ";
+                        if (s.Contains("'") == true)
+                        {
+                            string t = s.Replace("'", "''");
+                            temp = temp + "categories LIKE '%" + t + "%'" + " AND ";
+                        }
+                        else
+                        {
+                            temp = temp + "categories LIKE '%" + s + "%'" + " AND ";
+                        }
                     }
                     string categoryquery = temp.Remove(temp.Length - 5, 5);
-                    string sqlstr = "SELECT distinct businessname, city, state, temp.business_id FROM (SELECT distinct * FROM business WHERE city = '" + CityBox.SelectedItem.ToString() + "' AND state = '" + StateList.SelectedItem.ToString() + "' AND zipcode = '" + ZipcodeListBox.SelectedItem.ToString() + "') temp, hascategory WHERE temp.business_id=hascategory.business_id AND " + categoryquery;
+                    string sqlstr = "SELECT distinct business.businessname, address, city, state, zipcode, rating, tipcount, checkincount, business.business_id FROM (SELECT distinct businessname, array_to_string(array_agg(categoryname), ' , ') AS categories FROM(SELECT distinct * FROM business WHERE city = '" + CityBox.SelectedItem.ToString() + "' AND state = '" + StateList.SelectedItem.ToString() + "' AND zipcode = '" + ZipcodeListBox.SelectedItem.ToString() + "') temp, hascategory WHERE temp.business_id = hascategory.business_id GROUP BY businessname) temp1, business WHERE temp1.businessname = business.businessname AND city = '" + CityBox.SelectedItem.ToString() + "' AND state = '" + StateList.SelectedItem.ToString() + "' AND zipcode = '" + ZipcodeListBox.SelectedItem.ToString() + "' AND " + categoryquery;
                     executeQuery(sqlstr, AddGridRow);
                 }
                 else
                 {
-                    string sqlstr = "SELECT distinct businessname, city, state, temp.business_id FROM (SELECT distinct * FROM business WHERE city = '" + CityBox.SelectedItem.ToString() + "' AND state = '" + StateList.SelectedItem.ToString() + "' AND zipcode = '" + ZipcodeListBox.SelectedItem.ToString() + "') temp, hascategory WHERE temp.business_id=hascategory.business_id";
+                    string sqlstr = "SELECT distinct businessname, address, city, state, zipcode, rating, tipcount, checkincount, temp.business_id FROM (SELECT distinct * FROM business WHERE city = '" + CityBox.SelectedItem.ToString() + "' AND state = '" + StateList.SelectedItem.ToString() + "' AND zipcode = '" + ZipcodeListBox.SelectedItem.ToString() + "') temp, hascategory WHERE temp.business_id=hascategory.business_id";
                     executeQuery(sqlstr, AddGridRow);
+                }
+            }
+        }
+
+        private void ResultsGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ResultsGrid.SelectedIndex > -1)
+            {
+                Business B = ResultsGrid.Items[ResultsGrid.SelectedIndex] as Business;
+                if ((B.bid != null) && (B.bid.ToString().CompareTo("") != 0))
+                {
+                    Tips tipsWindow = new Tips(B.bid.ToString(), B.name.ToString());
+                    tipsWindow.Show();
                 }
             }
         }
---
+++ TermProject/Tips.xaml.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Npgsql;
+
+namespace TermProject
+{
+    /// <summary>
+    /// Interaction logic for Tips.xaml
+    /// </summary>
+    public partial class Tips : Window
+    { 
+        public class BusinessTips
+        {
+            public string date { get; set; }
+            public string name { get; set; }
+            public string likes { get; set; }
+            public string text { get; set; }
+        }
+
+        private string bid = "";
+        public Tips(string bid, string bname)
+        {
+            InitializeComponent();
+            this.bid = String.Copy(bid);
+            this.Title = "Tips by Users for " + bname;
+            AddColumns2Grid();
+            LoadTips();
+        }
+
+        private string buildConnectionString()
+        {
+            return "Host = localhost; Username = postgres; Database = yelpdb; password=11587750";
+        }
+
+        private void executeQuery(string sqlstr, Action<NpgsqlDataReader> myf)
+        {
+            using (var connection = new NpgsqlConnection(buildConnectionString()))
+            {
+                connection.Open();
+                using (var cmd = new NpgsqlCommand())
+                {
+                    cmd.Connection = connection;
+                    cmd.CommandText = sqlstr;
+                    try
+                    {
+                        var reader = cmd.ExecuteReader();
+                        while (reader.Read())
+                        {
+                            myf(reader);
+                        }
+                    }
+                    catch (NpgsqlException ex)
+                    {
+                        Console.WriteLine(ex.Message.ToString());
+                        System.Windows.MessageBox.Show("SQL Error - " + ex.Message.ToString());
+                    }
+                    finally
+                    {
+                        connection.Close();
+                    }
+                }
+            }
+        }
+
+        private void AddColumns2Grid()
+        {
+            DataGridTextColumn col1 = new DataGridTextColumn();
+            col1.Header = "Date";
+            col1.Binding = new Binding("date");
+            col1.Width = 140;
+            TipsGrid.Columns.Add(col1);
+
+            DataGridTextColumn col2 = new DataGridTextColumn();
+            col2.Header = "User Name";
+            col2.Binding = new Binding("name");
+            col2.Width = 100;
+            TipsGrid.Columns.Add(col2);
+
+            DataGridTextColumn col3 = new DataGridTextColumn();
+            col3.Header = "Likes";
+            col3.Binding = new Binding("likes");
+            col3.Width = 50;
+            TipsGrid.Columns.Add(col3);
+
+            DataGridTextColumn col4 = new DataGridTextColumn();
+            col4.Header = "Text";
+            col4.Binding = new Binding("text");
+            col4.Width = 600;
+            TipsGrid.Columns.Add(col4);
+        }
+
+        private void AddTipGrid(NpgsqlDataReader R)
+        {
+            TipsGrid.Items.Add(new BusinessTips() { date = R.GetDateTime(0).ToString(), name = R.GetString(1).ToString(), likes = R.GetInt16(2).ToString(), text = R.GetString(3).ToString() });
+        }
+
+        private void LoadTips()
+        {
+            string sqlstr = "SELECT tipdate, username, likecount, tiptext FROM (SELECT business_id, tipdate, username, likecount, tiptext FROM yelpuser, tip WHERE yelpuser.user_id = tip.user_id) temp WHERE business_id='" + this.bid +  "'";
+            executeQuery(sqlstr, AddTipGrid);
+        }
+
+        private void InsertTip(NpgsqlDataReader R)
+        {
+        }
+
+        private void AddTipButton_Click(object sender, RoutedEventArgs e)
+        {
+            string sqlstr = "INSERT INTO tip(business_id, tipdate, likecount, tiptext, user_id) VALUES('"+this.bid+ "',CURRENT_TIMESTAMP,0,'" + TextBox.Text.ToString()+"','jRyO2V1pA4CdVVqCIOPc1Q');";
+            executeQuery(sqlstr, InsertTip);
+            TipsGrid.Items.Clear();
+            LoadTips();
+        }
+    }
+}
